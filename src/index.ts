f721f7cbaca4b9eb8f0325ced67c3dde07296831
Frédérique Mittelstaedt <git@gfm.io>
2024-01-17 14:56:27 +0000

    update package exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export { AlreadySpawnedError } from "./AlreadySpawnedError";
+export { EmptyCommandError } from "./EmptyCommandError";
+export { ExitedWithExitCodeError } from "./ExitedWithExitCodeError";
+export { KilledWithSignalError } from "./KilledWithSignalError";
+export { NotYetSpawnedError } from "./NotYetSpawnedError";
+export { Spawnable } from "./Spawnable";
+export { Spawned } from "./Spawned";
